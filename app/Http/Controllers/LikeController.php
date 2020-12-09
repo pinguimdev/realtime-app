bd081a5d9adb295aa96750fac9514da6b9f4beb2
Werberson do Carmo <werberson00@gmail.com>
2020-12-09 15:41:45 -0300

    first commit

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Like;
+use App\Model\Reply;
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function likeIt(Reply $reply)
+    {
+        $reply->like()->create([
+            'user_id' => '1'
+        ]);
+    }
+
+    public function unLikeIt(Reply $reply)
+    {
+        $reply->like()->where(['user_id' => auth()->id()])->first()->delete();
+    }
+}
---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\QuestionResource;
+use App\Model\Question;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return QuestionResource::collection(Question::latest()->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Question::create($request->all());
+        return response()->json('Created', Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Question $question)
+    {
+        return new QuestionResource($question);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Question $question)
+    {
+        $question->update($request->all());
+        return response('Update', Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Question  $question
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Question $question)
+    {
+        $question->delete();
+        return response()->json('Deleted', 201);
+    }
+}
